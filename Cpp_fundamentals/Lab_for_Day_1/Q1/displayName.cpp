c45f1ec3ba93405f24ad5c3e5f49666e01edb2b6
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-01-25 15:54:03 +0000

    Added Cpp_Fundamentals

---
+++ Cpp_fundamentals/Lab_for_Day_1/Q1/displayName.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string name = "Elvis";
+	cout <<"My name is "<< name << endl;
+	return 0;
+
+}
---
+++ Cpp_fundamentals/try.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string phase = "Girafe Academy";
+	cout << "Welcome to "<< phase <<", my friend"<<endl;
+
+	return 0;
+}
